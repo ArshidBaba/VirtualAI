f0ec1270f0a42db9d6b35d9eb5337bcb0c9e6e41
Arshid Baba <arshidbaba93@gmail.com>
2022-02-14 21:33:22 +0530

    added song playing feature

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 import speech_recognition as sr
-
+import pyttsx3
+import pywhatkit
 listner = sr.Recognizer()
-try:
-    with sr.Microphone() as source:
-        print("listening...")
-        voice = listner.listen(source)
-        print("passed")
-        command = listner.recognize_google(voice)
-        command = command.lower()
-        if 'feinman' in command:
-            print(command)
-except:
-    pass
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():    
+    try:
+        with sr.Microphone() as source:
+            print("listening...")
+            voice = listner.listen(source)
+            print("passed")
+            command = listner.recognize_google(voice)
+            command = command.lower()
+            if 'jack' in command:
+                command = command.replace("jack", '')
+                print(command)
+    except:
+        pass
+    return command
+
+def run_feinman():
+    # engine.say("Hello Arshid")
+    # engine.say('What can I do for you sir?')
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+
+
+run_feinman()
